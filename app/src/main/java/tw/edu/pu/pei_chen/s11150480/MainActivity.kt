657e4acbf85ffe1b7e924f7f0fcfeb237c7073ac
pei chen <yukali58822@gmail.com>
2024-05-22 16:44:57 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/pu/pei_chen/s11150480/MainActivity.kt
@@ -0,0 +1,46 @@
+package tw.edu.pu.pei_chen.s11150480
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.pei_chen.s11150480.ui.theme.S11150480Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            S11150480Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S11150480Theme {
+        Greeting("Android")
+    }
+}
